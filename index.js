7e28c8a10d6c4fea0c75cea502825e634c77961f
borissvoboda <svoboda.boris@gmail.com>
2024-08-12 22:33:54 +0200

    id

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected with socket ID:', socket.id); // pretty cool!
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    // io.emit -  sends an event to everyone
+    io.emit('chat message', msg);
+
+    // io.to - sends message to specific socket.id
+    io.to(socket.id).emit(
+      'privateMessage',
+      'SERVER msg: Hello! Your id is: ' + socket.id
+    );
+  });
 });
 
 server.listen(3000, () => {
